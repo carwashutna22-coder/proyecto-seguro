5b882b300b583210c5e67eeb9af06414d50ee166
Josue Navarro <2236001315@utna.edu.mx>
2026-04-16 14:22:42 -0600

    fix middleware auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,9 @@
 'use strict';
 
-function protegerAdmin(req, res, next) {
+exports.protegerAdmin = (req, res, next) => {
   if (!req.session || !req.session.usuario) {
     return res.redirect('/login');
   }
-  next();
-}
 
-module.exports = { protegerAdmin };
+  next();
+};
