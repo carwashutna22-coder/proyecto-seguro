b17f70c8136f6e90fd67f8fdc8ef49e47b4034aa
Josue Navarro <2236001315@utna.edu.mx>
2026-04-16 14:37:01 -0600

    mensaje de cambio

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,11 @@
-'use strict';
-
 exports.protegerAdmin = (req, res, next) => {
+  console.log("SESSION:", req.session);
+
   if (!req.session || !req.session.usuario) {
+    console.log("❌ NO HAY SESIÓN");
     return res.redirect('/login');
   }
 
+  console.log("✅ SESIÓN OK");
   next();
 };
